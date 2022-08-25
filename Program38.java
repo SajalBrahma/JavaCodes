26c17cd262747ef7efccedc35ea544c72e581ecf
SAJAL BRAHMA <100282172+SajalBrahma@users.noreply.github.com>
2022-08-25 13:03:42 +0530

    Add files via upload

---
+++ Program38.java
@@ -0,0 +1,19 @@
+// To print the reverse of teh entered number
+import java.util.Scanner;
+public class Program38 
+{
+    public static void main(String[] args) 
+    {
+        Scanner obj=new Scanner(System.in);
+        System.out.print("Enter number :- ");
+        int num=obj.nextInt();
+        int rem=0,rev=0;
+        while(num!=0)
+        {
+            rem=num%10;
+            rev=(rev*10)+rem;
+            num=num/10;
+        }
+        System.out.println("Reverse Number :- "+rev);        
+    } 
+}
